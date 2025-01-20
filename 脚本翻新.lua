ddfbc16367685b5c15fdfe88d5310c49f102833b
qwaszxcv85 <1050754927@qq.com>
2025-01-20 17:01:34 +0800

    Update 脚本翻新.lua

--- 脚本翻新.lua
+++ 脚本翻新.lua
@@ -99,7 +99,7 @@ section:Button("光影", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/jHBfJYmS"))()
 end)
 local section = Tab:section("玩家更改",false)         
-section:Slider("跳跃高度", "", 1, 1, 100, false, function(Value)
+section:Slider("跳跃高度", "", 30, 1, 100, false, function(Value)
      print(Value)
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value  
   end)
