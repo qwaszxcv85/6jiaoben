e8890d8d77de6d63f01a41028f42ccb750222eac
qwaszxcv85 <1050754927@qq.com>
2025-01-21 14:14:42 +0800

    Update 脚本翻新.lua

--- 脚本翻新.lua
+++ 脚本翻新.lua
@@ -107,11 +107,11 @@ section:Button("光影", function()
 loadstring(game:HttpGet("https://pastebin.com/raw/jHBfJYmS"))()
 end)
 local section = Tab:section("玩家更改",false)         
-section:Slider("跳跃高度", "", 1, 50, 400, false, function(Value)
+section:Slider("跳跃高度", "", 50, 50, 400, false, function(Value)
      print(Value)
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value  
   end)
-section:Slider("移动速度", "", 1, 16, 400, false, function(Value)
+section:Slider("移动速度", "", 16, 16, 400, false, function(Value)
      print(Value)
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
               end)                                                                                     local Tab = window:Tab("dex和spy")         local section = Tab:section("dex",false)
