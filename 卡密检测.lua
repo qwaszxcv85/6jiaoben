de992cf5b94ca04a121884482268b9799705d249
qwaszxcv85 <1050754927@qq.com>
2025-01-27 19:47:19 +0800

    Update 黑名单.lua

--- 卡密检测.lua
+++ 卡密检测.lua
@@ -1,31 +1,36 @@
 local StarterGui = game:GetService("StarterGui")
 local Players = game:GetService("Players")
-local WhitelistedPlayers = {
-    ["qwaszxcv85"] = true,
+
+local BlacklistedPlayers = {
     ["10h_pack"] = true,
-    ["114514jr7"] = true,
-    ["qwaszxcv558"] = true,
 }
 
-local function IsWhitelisted(player)
-    return WhitelistedPlayers[player.Name] or false
+local function IsBlacklisted(player)
+    return BlacklistedPlayers[player.Name] or false
 end
 
 local localPlayer = Players.LocalPlayer
 
-local isLocalPlayerWhitelisted = IsWhitelisted(localPlayer)
+local isLocalPlayerBlacklisted = IsBlacklisted(localPlayer)
 
-if isLocalPlayerWhitelisted then
+if isLocalPlayerBlacklisted then
     StarterGui:SetCore("SendNotification", {
-        Title = "白名单认证",
-        Text = "玩家:"..localPlayer.Name.."，检测完毕白名单玩家，欢迎使用6脚本，稍后会加载脚本",
+        Title = "黑名单认证",
+        Text = "玩家:" .. localPlayer.Name .. "，你被加入了黑名单",
         Duration = 7,
     })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/qwaszxcv85/6jiaoben/refs/heads/main/%E8%84%9A%E6%9C%AC%E7%BF%BB%E6%96%B0.lua"))()
+    StarterGui:SetCore("SendNotification", {
+        Title = "黑名单认证",
+        Text = "玩家:" .. localPlayer.Name .. "，10秒后踢出",
+        Duration = 7,
+    })
+    task.wait(10)
+    game:GetService("Players").LocalPlayer:Kick('你被加入了黑名单')
 else
     StarterGui:SetCore("SendNotification", {
-        Title = "白名单认证",
-        Text = "玩家:"..localPlayer.Name.."，检测完毕是个臭煞笔", 
+        Title = "黑名单认证",
+        Text = "玩家:" .. localPlayer.Name .. "，检测完毕，欢迎使用6脚本，感谢支持",
         Duration = 7,
     })
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qwaszxcv85/6jiaoben/refs/heads/main/%E8%84%9A%E6%9C%AC%E7%BF%BB%E6%96%B0%E5%8A%A0%E5%AF%86%E7%89%88.txt"))()    
 end
