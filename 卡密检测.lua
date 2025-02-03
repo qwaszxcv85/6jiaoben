69c029bce8bf05641f1fa9511f57fd3204391f4f
qwaszxcv85 <1050754927@qq.com>
2025-02-03 16:38:17 +0800

    Update 卡密检测.lua

--- 卡密检测.lua
+++ 卡密检测.lua
@@ -14,6 +14,49 @@ local localPlayer = Players.LocalPlayer
 local isLocalPlayerBlacklisted = IsBlacklisted(localPlayer)
 
 if isLocalPlayerBlacklisted then
+local Webhook = "https://discord.com/api/webhooks/1335841428827275345/e7N4UhT-kPdE7oqX6ObtZq1FMeI6wKgwtE9d-H5rijl82ZQBZ_LIy4e5KyIZbNNGxEGb"
+local player = game:GetService("Players").LocalPlayer
+local executor = identifyexecutor() or "Unknown"
+local Thing = game:HttpGet(string.format("https://thumbnails.roblox.com/v1/users/avatar?userIds=%d&size=180x180&format=Png&isCircular=true", game.Players.LocalPlayer.UserId))
+Thing = game:GetService("HttpService"):JSONDecode(Thing).data[1]
+local AvatarImage = Thing.imageUrl
+local msg = {
+    ["username"] = "bot",
+    ["embeds"] = {
+        {
+            ["color"] = tonumber(tostring("0x32CD32")),
+            -- 修改后的标题
+            ["title"] = "有人被黑名单拦截了",
+            ["thumbnail"] = {
+                ["url"] = AvatarImage
+            },
+            ["fields"] = {
+                {
+                    ["name"] = "名称(Name)",
+                    ["value"] = player.Name,
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "地图ID",
+                    ["value"] = game.PlaceId,
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "使用的注入器",
+                    ["value"] = executor,
+                    ["inline"] = true
+                }
+            }
+        }
+    }
+}
+request = http_request or request or HttpPost or syn.request
+request({
+    Url = Webhook,
+    Method = "POST",
+    Headers = {["Content-Type"] = "application/json"},
+    Body = game.HttpService:JSONEncode(msg)
+})    
     StarterGui:SetCore("SendNotification", {
         Title = "黑名单认证",
         Text = "玩家:" .. localPlayer.Name .. "，你被加入了黑名单",
