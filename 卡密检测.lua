6da1dd6936b6d7f56d1739c8cf421f5ed9cef27e
qwaszxcv85 <1050754927@qq.com>
2025-02-03 16:39:03 +0800

    Update 卡密检测.lua

--- 卡密检测.lua
+++ 卡密检测.lua
@@ -2,7 +2,7 @@ local StarterGui = game:GetService("StarterGui")
 local Players = game:GetService("Players")
 
 local BlacklistedPlayers = {
-    ["10h"] = true,
+    ["10h_pack"] = true,
 }
 
 local function IsBlacklisted(player)
